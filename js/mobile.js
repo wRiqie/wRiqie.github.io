2d32968d4f95df594333a89219af5919008b5704
Henrique Moisés Fernandes <henriquegame512@outlook.com>
2021-01-25 20:15:29 -0300

    atualização

--- js/mobile.js
+++ js/mobile.js
@@ -1,41 +1,64 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
     "use strict";
-  
-    var navclone = function () {
-      $(".js-clone-nav").each(function () {
-        var $this = $(this);
-        $this.clone().attr("class", "clone-view").appendTo(".mobile-view-body");
-      });
 
-      $("body").on("click", ".js-toggle", function (e) {
-        var $this = $(this);
-        e.preventDefault();
-  
-        if ($("body").hasClass("off-view")) {
-          $("body").removeClass("off-view");
-        } else {
-          $("body").addClass("off-view");
-        }
-      });
-  
-      $(document).mouseup(function (e) {
-        var container = $(".mobile-view");
-        if (!container.is(e.target) && container.has(e.target).length === 0) {
-          if ($("body").hasClass("off-view")) {
-            $("body").removeClass("off-view");
-          }
-        }
-      });
-  
-      $(window).resize(function () {
-        var $this = $(this),
-          w = $this.width();
-        if (w > 768) {
-          if ($("body").hasClass("off-view")) {
-            $("body").removeClass("off-view");
-          }
-        }
-      });
+    var navclone = function() {
+        $(".js-clone-nav").each(function() {
+            var $this = $(this);
+            $this.clone().attr("class", "clone-view").appendTo(".mobile-view-body");
+        });
+
+        $("body").on("click", ".js-toggle", function(e) {
+            var $this = $(this);
+            e.preventDefault();
+
+            if ($("body").hasClass("off-view")) {
+                $("body").removeClass("off-view");
+            } else {
+                $("body").addClass("off-view");
+            }
+        });
+
+        // FECHA O "X"
+        $("body").on("click", "#close", function(e) {
+            var $this = $(this);
+            e.preventDefault();
+
+            if ($("body").hasClass("off-view")) {
+                $("body").removeClass("off-view");
+            } else {
+                $("body").addClass("off-view");
+            }
+        });
+
+        // FECHA CLICANDO NA PARTE BRANCA
+        $(document).mouseup(function(e) {
+            var container = $(".mobile-view");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($("body").hasClass("off-view")) {
+                    $("body").removeClass("off-view");
+                }
+            }
+        });
+
+        // FECHA O ESPAÇO EM BRANCO
+        $(document).mouseup(function(e) {
+            var container = $("a .nav-link");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($("body").hasClass("off-view")) {
+                    $("body").removeClass("off-view");
+                }
+            }
+        });
+
+        $(window).resize(function() {
+            var $this = $(this),
+                w = $this.width();
+            if (w > 768) {
+                if ($("body").hasClass("off-view")) {
+                    $("body").removeClass("off-view");
+                }
+            }
+        });
     };
     navclone();
-  });
+});
