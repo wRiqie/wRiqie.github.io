acb0ffc608d045df0e4a0b741c1db95772f854d6
Henrique Moisés Fernandes <henriquegame512@outlook.com>
2021-02-20 15:36:23 -0300

    Atualização
    - Adicionado SCSS
    - Bugs Removidos (Seção conhecimentos)
    -Ajustes em espaçamentos

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
@@ -6,4 +6,29 @@ window.addEventListener("scroll", function() {
 function toggle() {
     var header = document.getElementById('header');
     header.classList.toggle('active')
+}
+
+function showAll() {
+    const conhecimento = document.querySelectorAll('#conhecimento')
+    const showC = document.querySelector('#showC')
+    const hideC = document.querySelector('#hideC')
+
+    conhecimento.forEach(e => {
+        e.style.display = 'block'
+    })
+    showC.style.display = 'none'
+    hideC.style.display = 'block'
+}
+
+function hideAll() {
+    const conhecimento = document.querySelectorAll('#conhecimento')
+    const showC = document.querySelector('#showC')
+    const hideC = document.querySelector('#hideC')
+
+    conhecimento.forEach(e => {
+        e.style.display = 'none'
+    })
+    
+    showC.style.display = 'block'
+    hideC.style.display = 'none'
 }
