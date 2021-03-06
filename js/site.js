667a5f477988692e374b50370a16dbe992d376b6
wriqie <henrique5121@outlook.com>
2021-03-06 10:10:53 -0300

    Conhecimentos coloridos

--- js/site.js
+++ js/site.js
@@ -10,11 +10,16 @@ function toggle() {
 
 function showAll() {
     const conhecimento = document.querySelectorAll('#conhecimento')
+    const color = document.querySelectorAll('#colorido')
     const showC = document.querySelector('#showC')
     const hideC = document.querySelector('#hideC')
 
     conhecimento.forEach(e => {
         e.style.display = 'block'
+    })
+
+    color.forEach(e => {
+        e.style.filter = 'none'
     })
     showC.style.display = 'none'
     hideC.style.display = 'block'
@@ -22,11 +27,16 @@ function showAll() {
 
 function hideAll() {
     const conhecimento = document.querySelectorAll('#conhecimento')
+    const color = document.querySelectorAll('#colorido')
     const showC = document.querySelector('#showC')
     const hideC = document.querySelector('#hideC')
 
     conhecimento.forEach(e => {
         e.style.display = 'none'
+    })
+
+    color.forEach(e => {
+        e.style.filter = 'grayscale(1)'
     })
     
     showC.style.display = 'block'
